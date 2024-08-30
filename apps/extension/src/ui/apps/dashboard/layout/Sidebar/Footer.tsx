7532a79ef43f62464b3a6d9df3f344bc467a0ef0
akshatsynkcode <akshat@finloge.com>
2024-08-31 02:32:40 +0530

    first commit

---
+++ apps/extension/src/ui/apps/dashboard/layout/Sidebar/Footer.tsx
@@ -0,0 +1,7 @@
+import { FullColorLogo, FullColorVerticalLogo, HandRedLogo } from "@talisman/theme/logos"
+
+export const Footer = () => (
+  <footer className="flex w-full items-center justify-center p-8 md:p-12 lg:justify-between">
+   <img src="logo.svg" alt="" className="logo-white"/>
+  </footer>
+)
